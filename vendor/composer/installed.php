22ff574d6d528c2180a258ac2b7a51ec2b7e9ae2
fferrero <franck.ferrero@em-strasbourg.eu>
2025-03-13 17:23:52 +0100

    init

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'madbrain67/datatable',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'name' => 'plateformweb/datatable',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1d8cf97bccc72d67dc3c374f3c5a8884fd9bdd51',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'madbrain67/datatable' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+        'plateformweb/datatable' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1d8cf97bccc72d67dc3c374f3c5a8884fd9bdd51',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
